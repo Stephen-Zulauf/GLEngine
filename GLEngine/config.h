75281df6789c9d28d903991fa8eb8add5e3e7615
Stephen-Zulauf <szulauf1990@gmail.com>
2024-04-02 17:45:05 -0700

    init

---
+++ GLEngine/config.h
@@ -0,0 +1,13 @@
+#pragma once
+//opengl headers
+#include <glad/glad.h>
+//sdlHeaders
+#include <SDL/sdl.h>
+//glm headers
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
